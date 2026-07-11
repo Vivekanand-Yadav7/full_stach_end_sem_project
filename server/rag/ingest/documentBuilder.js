d18258c2063be7fa681341205194d7ca7c3ad7c5
Vivekanand-Yadav7 <vivekanand_yadav@srmap.edu.in>
2026-07-11 13:52:32 +0530

    feat: created function to convert postgress model to document

---
+++ server/rag/ingest/documentBuilder.js
@@ -0,0 +1,23 @@
+export const createProductDocument = (product) => {
+    return `
+Product Name: ${product.name}
+
+Description:
+${product.description || "Not provided"}
+
+Ingredients:
+${product.ingredients?.join("\n") || "Not provided"}
+
+Nutrition:
+${product.nutrition?.join("\n") || "Not provided"}
+
+Category:
+${product.category || "Not provided"}
+
+Tags:
+${product.tags?.join("\n") || "Not provided"}
+
+Price:
+₹${product.price}
+`.trim();
+};
